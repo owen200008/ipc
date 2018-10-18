a26a1556049f666490da98dd6fc0eef9ec0c7a9d
owen200008 <89424516@qq.com>
2018-10-18 15:47:25 +0800

    fix bug

--- unittest/ZIPC/client.cpp
+++ unittest/ZIPC/client.cpp
@@ -1,34 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <thread>
+#include "ZIPC/net/NetMgr.h"
+#include "ZIPC/net/TcpClient.h"
 
+int main() {
+    //must init before use net
+    zilliz::lib::NetMgr::GetInstance().Initialize();
+    do {
 
-//func call is success
-#define PrintSuccessOrFail(Func)\
-{\
-    char szFormat[128] = {'/'};\
-    int nRetValue = sprintf(szFormat, "F:%s", #Func);\
-    if(nRetValue > 0)\
-        for(int i = nRetValue;i < 64;i++)\
-            szFormat[i] = '/';\
-    printf(szFormat);\
-    printf("\n");\
-    if(Func())\
-        nRetValue = sprintf(szFormat, "R:Success");\
-    else\
-        nRetValue = sprintf(szFormat, "R:Fail???");\
-    if(nRetValue > 0)\
-        for(int i = nRetValue;i < 64;i++)\
-            szFormat[i] = '/';\
-    printf(szFormat);\
-    printf("\n");\
-}
+    } while (false);
 
-int main(){
-
-	PrintSuccessOrFail(SmartBufferTest);
-	PrintSuccessOrFail(IPCLogTest);
-	printf("End Test IPC\n");
-	getchar();
-	return 0;
+    getchar();
+    zilliz::lib::NetMgr::GetInstance().CloseNetSocket();
+    return 0;
 }
