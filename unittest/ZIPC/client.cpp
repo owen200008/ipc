142dffbb73fc25968292cf0d985780d0ba23b18b
owen200008 <89424516@qq.com>
2018-10-19 18:22:22 +0800

    add code

--- unittest/ZIPC/client.cpp
+++ unittest/ZIPC/client.cpp
@@ -169,7 +169,7 @@ int main() {
             zilliz::lib::BitStream sendBuffer;
             sendBuffer.SetDataLength(1024 * 1024 * 400);
             sendBuffer.SetDataLength(0);
-            for (uint32_t i = 0; i < 512; i++) {
+            for (uint32_t i = 0; i < 100; i++) {
                 sendBuffer << i;
             }
             for (int j = 0; j < 2; j++) {
